da2b8efb5292e6a6a911cefb494c064766ba22da
sbs-one <6616896@gmail.com>
2020-06-13 23:07:47 +0300

    first commit

---
+++ src/test/java/ru/deliveryClub/WebDriverSettings.java
@@ -0,0 +1,20 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class WebDriverSettings {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:/Chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
